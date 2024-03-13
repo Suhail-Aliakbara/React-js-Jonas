273c4b00a304ed5cea197872b5227853e474befb
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-03-13 22:27:09 +0530

    learned about how event works behind the scene

--- z4-how-react-works/src/App.js
+++ z4-how-react-works/src/App.js
@@ -39,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -65,6 +68,18 @@ function TabContent({ item }) {
   function handleInc() {
     setLikes(likes + 1);
   }
+  function handle3Inc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
 
   return (
     <div className="tab-content">
@@ -79,13 +94,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handle3Inc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
